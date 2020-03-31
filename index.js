892411254ac013f5a1f3959e50c65e1ae1c0f03e
AFlak <AndrewMFlak@gmail.com>
2020-03-31 16:51:01 -0400

    updated

--- index.js
+++ index.js
@@ -25,8 +25,27 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); 
+// This will emit the event to all connected sockets
+
+io.on('connection', function(socket){
+  socket.broadcast.emit('hi');
+});
+
+
+
+
