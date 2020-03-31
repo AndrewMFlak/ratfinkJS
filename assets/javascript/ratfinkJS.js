c0901e5a07a7d39fc54cede7fa81fe7bd3be452c
AFlak <AndrewMFlak@gmail.com>
2020-03-31 12:20:36 -0400

    updated to include deck cut

--- assets/javascript/ratfinkJS.js
+++ assets/javascript/ratfinkJS.js
@@ -2,34 +2,49 @@ var cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 var suits = ["diamonds", "hearts", "spades", "clubs"];
 var deck = new Array();
 
-function getDeck()
-{
+function getDeck() {
 	var deck = new Array();
 
-	for(var i = 0; i < suits.length; i++)
-	{
-		for(var x = 0; x < cards.length; x++)
-		{
-			var card = {Value: cards[x], Suit: suits[i]};
+	for (var i = 0; i < suits.length; i++) {
+		for (var x = 0; x < cards.length; x++) {
+			var card = { Value: cards[x], Suit: suits[i] };
 			deck.push(card);
 		}
 	}
-    console.log("deck log: " + deck)
+	console.log("deck log: " + deck)
 	return deck;
 }
 
-function shuffle()
-{
+function shuffle() {
 	// for 1000 turns
-    var deckCheckDiv = document.createElement("div");
-    deckCheckDiv.className = "DeckCheckDivValue";
-    //using innerHTML
-    deckCheckDiv.innerHTML += deck.length;
-    //using 
-    document.getElementById("deckCheck").appendChild(deckCheckDiv);
+	var deckCheckDiv = document.createElement("div");
+	//clear DeckCheckDivValue HERE
+	deckCheckDiv.className = "DeckCheckDivValue";
+	//using innerHTML
+	deckCheckDiv.innerHTML += deck.length;
+	//using 
+	document.getElementById("deckCheck").appendChild(deckCheckDiv);
 	// switch the values of two random cards
-	for (var i = 0; i < 1000; i++)
-	{
+	for (var i = 0; i < deck.length; i++) {
+		var location1 = Math.floor((Math.random() * deck.length));
+		var location2 = Math.floor((Math.random() * deck.length));
+		var tmp = deck[location1];
+
+		deck[location1] = deck[location2];
+		deck[location2] = tmp;
+	}
+	console.log("shuffle success");
+
+	renderDeck();
+}
+
+function cutDeck() {
+	// for 1000 turns
+	//using 
+	// switch the values of two random cards
+	var deckCut = Math.floor(Math.random() * deck.length)
+	for (var i = deckCut; i < deck.length; i++) {
+
 		var location1 = Math.floor((Math.random() * deck.length));
 		var location2 = Math.floor((Math.random() * deck.length));
 		var tmp = deck[location1];
@@ -38,48 +53,46 @@ function shuffle()
 		deck[location2] = tmp;
 	}
 
+	console.log("cut deck success")
+
 	renderDeck();
 }
 
-function renderDeck()
-{
+function renderDeck() {
 	document.getElementById('deck').innerHTML = '';
-	for(var i = 0; i < deck.length; i++)
-	{
+	for (var i = 0; i < deck.length; i++) {
 		var card = document.createElement("div");
 		var value = document.createElement("div");
 		var suit = document.createElement("div");
-        card.className = "card";
+		card.className = "card";
 		value.className = "value";
 		suit.className = "suit " + deck[i].Suit;
 
 		value.innerHTML = deck[i].Value;
 		card.appendChild(value);
 		card.appendChild(suit);
-        var img = document.createElement('img'); 
-        if (deck[i].Suit == 'hearts')
-		suitImage='../images/heartImage2.png';
+		var img = document.createElement('img');
+		if (deck[i].Suit == 'hearts')
+			suitImage = './assets/images/heartImage2.png';
 		else if (deck[i].Suit == 'spades')
-		suitImage = '../images/spadeImage2.png';
+			suitImage = './assets/images/spadeImage2.png';
 		else if (deck[i].Suit == 'diamonds')
-		suitImage = '../images/diamondImage.png';
+			suitImage = './assets/images/diamondImage.png';
 		else
-		suitImage = '../images/clubImage.png';
-        img.src = suitImage;
-        card.appendChild(img);
+			suitImage = './assets/images/clubImage.png';
+		img.src = suitImage;
+		card.appendChild(img);
 
 
 		document.getElementById("deck").appendChild(card);
 	}
 }
 
-function load()
-{
+function load() {
 	deck = getDeck();
 	shuffle();
 	renderDeck();
 }
 
 window.onload = load;
-                
-            
+
